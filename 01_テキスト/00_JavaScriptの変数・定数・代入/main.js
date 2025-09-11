93c145d73e705df5e55c885aae0305419743d227
kohei <pashina411@gmail.com>
2025-09-11 12:10:44 +0900

    [3の倍数]を学習しました。

--- 01_テキスト/00_JavaScriptの変数・定数・代入/main.js
+++ 01_テキスト/00_JavaScriptの変数・定数・代入/main.js
@@ -1,9 +1,10 @@
 let myNickname = "エキスパくん"
 
 console.log(myNickname) // => "エキスパくん" と表示される
-
+myNickname = "あなたのあだ名"
 // TRY: 変数 myNickname に再代入して自分のあだ名を表示してください
 
 console.log(myNickname)
 
-// TRY: myAge を定数で宣言して自分の年齢を表示してください
+const myAge = 20
+console.log(myAge)// TRY: myAge を定数で宣言して自分の年齢を表示してください
