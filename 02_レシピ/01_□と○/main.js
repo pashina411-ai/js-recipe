f121fe59e98df281c2d162a9a8c0a1baaa5222f3
kohei <pashina411@gmail.com>
2025-09-15 09:48:24 +0900

    [ストップウォッチ]を学習しました。

--- 02_レシピ/01_□と○/main.js
+++ 02_レシピ/01_□と○/main.js
@@ -1,5 +1,16 @@
-const figure = document.getElementById("figure")
+const figure = document.getElementById("figure");
+
+// クラスを順番に切り替えるリスト
+const shapes = ["square", "circle", "triangle"];
+let index = 0;
 
 figure.onclick = function () {
-  figure.classList.toggle("rounded")
-}
+  // 今のクラスを削除
+  figure.classList.remove(shapes[index]);
+
+  // 次の図形へ
+  index = (index + 1) % shapes.length;
+
+  // 新しいクラスを追加
+  figure.classList.add(shapes[index]);
+};
